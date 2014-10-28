1b1afe84d43b0e4aa31f87431f60f71e6160d32e
Kate Lindsay <einz3lhaft@gmail.com>
2014-10-28 11:56:40 -0400

    updated to wordpress

---
+++ wp-content/themes/MidnightStatic/index.php
@@ -0,0 +1,31 @@
+<?php
+
+get_header(); ?>
+
+
+<div class="container">
+	<section id="header">
+		<h1>Midnight Static</h1>
+		<div id="color-change"></div>
+	</section>
+	<section id="music">
+		<h1>Music</h1>
+		<p>Coming soon...</p>
+	</section>
+	<section id="about">
+		<h1>About</h1>
+		<p>Coming soon...</p>
+	</section>
+	<section id="shows">
+		<h1>Shows</h1>
+		<p>Coming soon...</p>
+	</section>
+	<section id="contact">
+		<h1>Contact</h1>
+		<p>Coming soon...</p>
+	</section>
+</div>
+
+<?php
+get_footer();
+?>
